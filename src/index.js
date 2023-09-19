0e1f7c0a3e0ffdd7e60d3516f4173d857d5e48e4
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-09-19 15:11:55 +0200

    fix: task morse-decoder is done (Tue, Sep 19, 2023 03:11:55 PM)

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+  for (let i = 0; i < expr.length; i = i + 10) {
+    const part = expr.slice(i, i + 10);
+    arr.push(part);
+  }
+
+  let morseSymbols = [];
+
+  for (let x = 0; x < arr.length; x++) {
+    let arr2 = [];
+    let symbol = [];
+
+    for (let i = 0; i < arr[x].length; i = i + 2) {
+      const part = arr[x].slice(i, i + 2);
+      arr2.push(part);
+    }
+
+    for (let v = 0; v < arr2.length; v++) {
+      if(arr2[v] === '10') {
+        symbol.push('.');
+        } else if (arr2[v] === '11') {
+          symbol.push('-');
+        } else if (arr2[v] === '**') {
+          symbol.push(' ');
+        }
+    }
+    let concatSymbols = symbol.join().replace(/[\s,%]/g, '');
+    morseSymbols.push(concatSymbols)
+  }
+
+  let sentense = [];
+
+  for (let f = 0; f < morseSymbols.length; f++) {
+    let cur = morseSymbols[f]
+    if(cur === '') {
+      sentense.push('$')
+    } else {sentense.push(MORSE_TABLE[cur])}
+  }
+
+  return sentense.join().replace(/[\s,%]/g, '').replaceAll('$', ' ')
 }
 
 module.exports = {
